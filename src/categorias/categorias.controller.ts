48426bacf835f102a399dbe06fb973654d6e3b25
alex13 <acedoalex13@gmail.com>
2025-11-27 11:51:38 -0500

    Este es el crud COMPLETO SOBRE CATEGORIAS

--- src/categorias/categorias.controller.ts
+++ src/categorias/categorias.controller.ts
@@ -1,13 +1,45 @@
-import { Controller, Get } from '@nestjs/common';
+import { Body, Controller, Delete, Get, Param,Patch, Post } from '@nestjs/common';
 import { CategoriasService } from './categorias.service';
+import { createCategoriaDTO } from './dto/create-categories.dto';
+import { UpdateCategoriaDto } from './dto/update-categories.dto';
 
 @Controller('categorias')
 export class CategoriasController {
 
+    //Conecta con el categoriaServices
     constructor(private categoriaService: CategoriasService) { }
 
+    //Ruta para listar todos los productos
     @Get()
     findAll() {
         return this.categoriaService.finAll();
     }
+
+
+    //ruta para buscar por id
+    @Get(':id')
+    finOne(@Param('id') id:number){
+        return this.categoriaService.findOne(id)
+    }
+
+    //Ruta para agregar
+    @Post()
+    crete(@Body() createCategoriaDTO:createCategoriaDTO){
+        return this.categoriaService.create(createCategoriaDTO)
+    }
+
+    //Ruta para actualizar
+    @Patch(':id')
+    update(@Param('id') id :string , @Body() UpdateCategoriaDto:UpdateCategoriaDto){
+        return this.categoriaService.update(+id,UpdateCategoriaDto)
+    }
+
+    //Ruta para eliminar
+
+    @Delete(':id')
+    remove(@Param('id') id :string){
+        return this.categoriaService.remove(+id)
+    }
+
+
 }
--- src/categorias/categorias.service.ts
+++ src/categorias/categorias.service.ts
@@ -1,7 +1,9 @@
-import { Injectable } from '@nestjs/common';
+import { ConflictException, Injectable, NotFoundException } from '@nestjs/common';
 import { InjectRepository } from '@nestjs/typeorm';
 import { Categoria } from './entity/categoria.entity';
 import { Repository } from 'typeorm';
+import { createCategoriaDTO } from './dto/create-categories.dto';
+import { UpdateCategoriaDto } from './dto/update-categories.dto';
 
 @Injectable()
 export class CategoriasService {
@@ -9,9 +11,71 @@ export class CategoriasService {
     constructor(@InjectRepository(Categoria) private categoriaRepositori: Repository<Categoria>) { }
 
 
+    //Logica para listar
     finAll() {
         return this.categoriaRepositori.find()
     }
+
+    //Logica para buscar por id
+    async findOne(id: number) {
+        const categoria = await this.categoriaRepositori.findOne({ where: { id } })
+        if (!categoria) {
+            throw new NotFoundException(`categoria con el id ${id} no encontrada`)
+        }
+
+        return categoria;
+    }
+
+    //Logica para crear
+    async create(createCategori: createCategoriaDTO) {
+        const existe = await this.categoriaRepositori.findOne({
+            where: { nombre: createCategori.nombre }
+        })
+        if (existe) {
+            throw new ConflictException(`Ya existe la categoria ${existe.nombre}`)
+        }
+
+        const categoria = this.categoriaRepositori.create(createCategori)
+        return this.categoriaRepositori.save(categoria)
+
+    }
+
+    //Logica para actualizar
+    async update(id: number, updateCategori: UpdateCategoriaDto) {
+        const categoria = await this.findOne(id);
+        if (UpdateCategoriaDto.name && UpdateCategoriaDto.name! == categoria.nombre) {
+            const existe = await this.categoriaRepositori.findOne({
+                where: {
+                    nombre: updateCategori.nombre
+                }
+            })
+
+            if (existe) {
+                throw new ConflictException('Ya existe una categoria con ese nombre')
+            }
+
+        }
+        Object.assign(categoria, updateCategori)
+        return this.categoriaRepositori.save(categoria)
+    }
+
+//Logica para eliminar
+   async remove(id: number) {
+    const category = await this.findOne(id);
+
+    if (category.productos && category.productos.length > 0) {
+      throw new ConflictException(
+        `No se puede eliminar la categoría porque tiene ${category.productos.length} producto(s) asociado(s)`,
+      );
+    }
+
+    await this.categoriaRepositori.remove(category);
+
+    return {
+      message: 'Categoría eliminada correctamente',
+      category,
+    };
+  }
 
 
 }
